4e56b130728ee32ed81a95471e818a7ba88030aa
tadkim <icanhear@dgu.edu>
2016-08-08 02:34:43 +0900

    init commit

---
+++ src/client/typings.d.ts
@@ -0,0 +1,2 @@
+/// <reference path="../../tools/manual_typings/project/index.d.ts"/>
+/// <reference path="../../typings/index.d.ts"/>
---
+++ tools/manual_typings/project/index.d.ts
@@ -0,0 +1,6 @@
+// declare module "moment/moment" {
+//   export = moment;
+// }
+
+declare var jQuery: any;
+declare var $: any;
